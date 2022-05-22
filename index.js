fd58000f68d59b121770e787d7027ca189aa023f
Al-Imran <alimran21feb@gmail.com>
2022-05-23 03:37:17 +0600

    connected with mongodb and added env file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hecqq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
