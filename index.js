7ab97acbf5040cbcbd9ac3c82634c7c49b29bf01
Al-Imran <alimran21feb@gmail.com>
2022-05-25 20:54:22 +0600

    added manage products api

--- index.js
+++ index.js
@@ -42,6 +42,20 @@ async function run() {
         const productsCollection = client.db('HAMMER').collection('products');
         const userCollection = client.db('HAMMER').collection('users');
         const bookingCollection = client.db('HAMMER').collection('bookings');
+        const addProductCollection = client.db('HAMMER').collection('addProducts');
+
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+        }
 
 
         app.get('/user', verifyJWT, async (req, res) => {
@@ -58,22 +72,15 @@ async function run() {
         });
 
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester });
 
-            if (requesterAccount.role === 'admin') {
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-                const result = await userCollection.updateOne(filter, updateDoc);
-                res.send(result);
-            }
-            else {
-                return res.status(403).send({ message: 'Forbidden access' });
-            }
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
 
         });
 
@@ -136,6 +143,29 @@ async function run() {
             const result = await bookingCollection.insertOne(booking);
             res.send({ success: true, result });
         });
+
+
+        app.get('/addProduct', verifyJWT, verifyAdmin, async (req, res) => {
+            const addProducts = await addProductCollection.find().toArray();
+            res.send(addProducts);
+        });
+
+
+        app.post('/addProduct', verifyJWT, verifyAdmin, async (req, res) => {
+            const product = req.body;
+            const result = await addProductCollection.insertOne(product);
+            res.send(result);
+        });
+
+
+
+        app.delete('/addProduct/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await addProductCollection.deleteOne(filter);
+            res.send(result);
+        });
+
 
     }
 
